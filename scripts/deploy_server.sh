bc2519dd44df53fc8bd4391dffa0b036726bad8a
Tim Kye <tyrsius@gmail.com>
2019-05-02 23:42:09 -0700

    add client deploy script

--- scripts/deploy_server.sh
+++ scripts/deploy_server.sh
@@ -11,6 +11,3 @@ cp ../.env ./terraform.tfvars
 terraform apply -auto-approve 
 rm ./terraform.tfvars
 popd
-
-# aws s3 sync src s3://lesswrong.kye.dev 
-# aws s3 cp src/index.html s3://lesswrong.kye.dev/index.html --cache-control max-age=0
